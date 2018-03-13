7b641447e4f93ea418680a1dc59b041bacb3a431
Bryn Thomas <bryn500@outlook.com>
2018-03-13 14:14:15 +0000

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using $safeprojectname$.Models;
+
+namespace $safeprojectname$.Controllers
+{
+    [ResponseCache(CacheProfileName = "Default")]
+    //[Route("")] // prefix
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            const string testSessionName = "testSession";
+            var serialisedDate = JsonConvert.SerializeObject(DateTime.Now);
+            HttpContext.Session.SetString(testSessionName, serialisedDate);
+
+            ViewData.Add("Title", "Homepage");
+
+            return View();
+        }
+
+        [HttpGet("problem")]
+        public IActionResult Problem()
+        {
+            return StatusCode(500);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+
+namespace $safeprojectname$
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // cache on server based on client cache rules
+            services.AddResponseCaching();
+
+            // client side caching profiles, keep small because no way to invalidate
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("Default",
+                    new CacheProfile()
+                    {
+                        Duration = 60
+                    });
+                options.CacheProfiles.Add("Never",
+                    new CacheProfile()
+                    {
+                        Location = ResponseCacheLocation.None,
+                        NoStore = true
+                    });
+            });
+
+            // Memory Caching and Session
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // output caching in production
+                app.UseResponseCaching();
+                
+                app.UseStatusCodePagesWithReExecute("/error/{0}");
+            }
+
+            // add cache control header to static resources
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                OnPrepareResponse = (context) =>
+                {
+                    var headers = context.Context.Response.GetTypedHeaders();
+                    headers.CacheControl = new CacheControlHeaderValue()
+                    {
+                        MaxAge = TimeSpan.FromDays(365)
+                    };
+                }
+            });
+
+            // IMPORTANT! enable session before MVC
+            app.UseSession();
+
+            // don't pass routes for Attribute routing
+            app.UseMvc();
+        }
+    }
+}
